8d8eaecbbbd7ed5329183d3401ffa1e85f66f502
qu1queee <encalada@de.ibm.com>
2021-01-14 22:03:36 +0100

    Refactor position root.go
    Move from pkg/shp/cmd to pkg/cmd

--- cmd/shp/main.go
+++ cmd/shp/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/shipwright-io/cli/pkg/shp/cmd"
+	"github.com/shipwright-io/cli/pkg/cmd"
 )
 
 func main() {
-	rootCmd := cmd.NewCmd()
+	rootCmd := cmd.NewSHPRootCmd()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Printf("[ERROR] %#v\n", err)
 		os.Exit(1)
---
+++ pkg/cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/shipwright-io/cli/pkg/cmd/build"
+	"github.com/shipwright-io/cli/pkg/cmd/buildrun"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when used without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "shp",
+	Short: "Command-line client for Shipwright Build.",
+	Long: `Command-line client for Shipwright Build
+
+shp allows you to build container images securely inside your 
+Kubernetes cluster.
+
+See the individual commands to get an overview.
+`,
+}
+
+func init() {
+	rootCmd.AddCommand(
+		build.BuildCmd,
+		buildrun.BuildRunCmd,
+	)
+}
+
+// NewSHPRootCmd returns the cobra base cmd
+func NewSHPRootCmd() *cobra.Command {
+	return rootCmd
+}
